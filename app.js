c8995f15efe754e492d2da58273430b3acc0ca0b
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2020-04-09 18:25:47 -0300

    configurações iniciais do projeto

---
+++ app.js
@@ -0,0 +1,10 @@
+var message = 'Help me, Obi-Wan Kenobi. You\'re my only hope!';
+console.log(message);
+var episode = 4;
+console.log("This is episode " + episode);
+episode += 1;
+console.log("Next episode is " + episode);
+var favoriteDroid;
+favoriteDroid = 'BB-8';
+console.log("My favorite Droid is " + favoriteDroid);
+//# sourceMappingURL=app.js.map
