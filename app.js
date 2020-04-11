cac46b0bff8c49210891e678403059c1573dfc05
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2020-04-10 22:14:38 -0300

    added nodemon dev dependency

--- app.js
+++ app.js
@@ -7,4 +7,9 @@ console.log("Next episode is " + episode);
 var favoriteDroid;
 favoriteDroid = 'BB-8';
 console.log("My favorite Droid is " + favoriteDroid);
+var isEnoughToBeatMF = function (parsecs) {
+    return parsecs < 12;
+};
+var distance = 14;
+console.log("Is " + distance + " enough to  beat Millenium Falcon? " + isEnoughToBeatMF(distance));
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -9,3 +9,10 @@ console.log(`Next episode is ${episode}`)
 let favoriteDroid
 favoriteDroid = 'BB-8'
 console.log(`My favorite Droid is ${favoriteDroid}`)
+
+let isEnoughToBeatMF = function (parsecs: number) : boolean {
+    return parsecs < 12
+}
+
+let distance = 14
+console.log(`Is ${distance} enough to  beat Millenium Falcon? ${isEnoughToBeatMF(distance)}`)
