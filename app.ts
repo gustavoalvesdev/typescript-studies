1c8c6181bcffdf85c4968c151dac7ca0c9ae378a
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2020-04-10 22:26:16 -0300

    using functions in typescript

--- app.ts
+++ app.ts
@@ -16,3 +16,13 @@ let isEnoughToBeatMF = function (parsecs: number) : boolean {
 
 let distance = 14
 console.log(`Is ${distance} enough to  beat Millenium Falcon? ${isEnoughToBeatMF(distance)}`)
+
+let call = (name: String) => console.log(`Do you copy, ${name}?`)
+call('R2')
+
+function inc (speed: number, inc: number = 1) : number {
+    return speed + inc
+}
+
+console.log(`inc (5, 1) = ${inc(5, 1)}`)
+console.log(`inc (5) = ${inc(5)}`)
